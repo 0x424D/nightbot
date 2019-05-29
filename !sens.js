cf340f236ed895f93534c6ffb671bb4207e84ad8
0x424D <30124318+0x424D@users.noreply.github.com>
2019-05-29 19:46:43 +0100

    Fix indents

--- !sens.js
+++ !sens.js
@@ -1,16 +1,16 @@
 !editcom !sens $(eval
-    sensDict = $(urlfetch json https://raw.githubusercontent.com/0x424D/nightbot/master/dicts/sens);
+	sensDict = $(urlfetch json https://raw.githubusercontent.com/0x424D/nightbot/master/dicts/sens);
 	game = "$(1)";
 	if (game == "null")
 	{
 		sens["ow"];
 	}
-    else if (game in sens)
-    {
+	else if (game in sens)
+    	{
         sens[game];
-    }
-    else
-    {
-        "Game not found";
-    }
+    	}
+    	else
+    	{
+       		"Game not found";
+    	}
 )
